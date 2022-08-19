4b09f1f838158c6c04343504128f6f478099f91a
juan.prodater <juanvleal@gmail.com>
2022-08-19 11:32:09 -0300

    Adicionando tratamento de exceções

--- src/main/java/br/com/dio/service/ProdutoService.java
+++ src/main/java/br/com/dio/service/ProdutoService.java
@@ -2,6 +2,8 @@ package br.com.dio.service;
 
 import java.util.List;
 
+import br.com.dio.exception.ProductNullException;
+import br.com.dio.exception.ProductPriceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,7 +16,12 @@ public class ProdutoService {
 	@Autowired
 	private ProdutoRepository repository;
 
-	public Produto save(Produto produto) {
+	public Produto save(Produto produto) throws Exception {
+		if(produto.getNome() == null || produto.getPreco() == null)
+			throw new ProductNullException();
+		if(produto.getPreco() < 0)
+			throw new ProductPriceException();
+
 		return repository.save(produto);
 	}
 
